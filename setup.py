f380a4a9e3ca14ff8bc3ca53057d6997dd2c7c46
Joaquin Keller <joaquin.keller@gmail.com>
2020-07-15 10:32:27 +0800

    Updated to upload to PyPI

--- setup.py
+++ setup.py
@@ -2,14 +2,17 @@ from setuptools import setup, find_packages
 
 setup_args = dict(
     name='manyq',
-    version='0.0.0',
-    description='Fast quantum computer simulator for QML',
-    license='MIT',
+    version='0.0.1',
+    description='Fast quantum computer simulator for Quantum Machine Learning',
+    license='Apache 2.0',
     packages=find_packages(),
     author='Joaquin Keller',
     author_email='joaquin@entropicalabs.io',
     keywords=['qubit', 'quantum'],
-    url='https://entropicalabs.com'
+    url='https://github.com/entropicalabs/manyQ',
+    classifiers=[
+        'Development Status :: 4 - Beta', 'License :: OSI Approved :: Apache Software License','Programming Language :: Python :: 3'
+    ]
 )
 
 install_requires = [
